431bc108ba42b7b984a059eaed4cdf86fd715626
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-14 20:18:23 +0100

    Add register api call algorithm

---
+++ src/features/registerForm/registerFormApi.ts
@@ -0,0 +1,16 @@
+import { NewUser } from '../../models/user-model';
+
+export const registerUser = async (newUser: NewUser) => {
+  const response = await fetch(
+    'https://joaquin-godoy-final-project-back-202301.onrender.com/auth/register',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    }
+  );
+
+  return await response.status;
+};
