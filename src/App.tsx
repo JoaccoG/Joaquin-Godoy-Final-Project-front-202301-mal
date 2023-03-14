a18fccf0928a0cc94b0c21c62019fbd5a22a6a50
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-14 15:18:48 +0100

    Add fontawesome dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+
 const App = () => {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <FontAwesomeIcon icon={solid('gamepad')} />
     </div>
   );
 };
