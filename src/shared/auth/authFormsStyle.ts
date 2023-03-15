947db0f1cc56a1cfe0eafad7ac039e2720d93be2
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-15 18:39:54 +0100

    Move auth form styles to shared folder

---
+++ src/shared/auth/authFormsStyle.ts
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { RegisterStatus } from '../../features/registerForm/registerFormSlice';
+
+interface AuthStatusProps {
+  authStatus: RegisterStatus;
+}
+
+export const AuthFormContainer = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--margin-l);
+  padding: var(--padding-s);
+  input {
+    min-width: 200px;
+    width: 100%;
+    max-width: 500px;
+    font-size: var(--font-size-s);
+    font-family: var(--font-family-inter-bold);
+    font-weight: var(--font-weight-l);
+    color: var(--color-secondary);
+    background-color: var(--bg-color-tertiary);
+    padding: var(--padding-s) var(--padding-m);
+    border: none;
+    border-radius: var(--border-m);
+    transition: var(--transition-short);
+    &:focus {
+      outline: var(--color-success) 1px solid;
+      color: var(--color-primary);
+    }
+    &:required:invalid {
+      outline: var(--color-danger) 1px solid;
+    }
+    &:required:invalid:not(:focus) {
+      outline: none;
+    }
+    &:required:valid {
+      outline: var(--color-success) 1px solid;
+    }
+  }
+  .submit-btn {
+    width: 200px;
+    font-size: var(--font-size-m);
+    font-family: var(--font-family-inter-bold);
+    font-weight: var(--font-weight-l);
+    color: var(--color-primary);
+    background-color: var(--color-tertiary);
+    padding: var(--padding-xs) var(--padding-m);
+    border: none;
+    border-radius: var(--border-m);
+    transition: var(--transition-short);
+    &:hover {
+      cursor: pointer;
+      background-color: var(--color-tertiary-hover);
+      transform: scale(1.05);
+    }
+  }
+`;
+
+export const AuthStatusFeedback = styled.div<AuthStatusProps>`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--margin-s);
+  padding: var(--padding-s);
+  text-align: center;
+  color: var(--color-secondary);
+  font-size: var(---font-size-s);
+  color: ${(props) => {
+    switch (props.authStatus) {
+      case 'idle':
+        return 'var(--color-secondary)';
+      case 'success':
+        return 'var(--color-success)';
+      default:
+        return 'var(--color-danger)';
+    }
+  }};
+`;
