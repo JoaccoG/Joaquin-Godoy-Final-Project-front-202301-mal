7db9e3ad114267e815126c310b325296f8df70d3
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-16 00:59:55 +0100

    Remove unused api call

--- src/features/Users/Auth/auth-api.ts
+++ src/features/Users/Auth/auth-api.ts
@@ -14,18 +14,3 @@ export const registerUser = async (newUser: UserCredentials) => {
 
   return response;
 };
-
-export const loginUser = async (newUser: UserCredentials) => {
-  const response = await fetch(
-    'https://joaquin-godoy-final-project-back-202301.onrender.com/auth/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newUser),
-    }
-  );
-
-  return response;
-};
