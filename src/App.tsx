78ed8731139aa484fbc25f443b5b4e03bddb2a51
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-15 15:58:19 +0100

    Add missing msw handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
-
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-      <FontAwesomeIcon icon={solid('gamepad')} />
-    </div>
+    <main className="App">
+      <h1>PlayersNation</h1>
+    </main>
   );
 };
 
