46fb536f82f96138b38f4a549beff0be1dd7bf23
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-15 15:19:02 +0100

    Add register styles to feedback message

--- src/features/registerForm/registerFormStyle.ts
+++ src/features/registerForm/registerFormStyle.ts
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import { RegisterStatus } from './registerFormSlice';
+
+interface RegisterStatusProps {
+  registerStatus: RegisterStatus;
+}
 
 export const RegisterFormContainer = styled.form`
   width: 100%;
@@ -22,8 +27,20 @@ export const RegisterFormContainer = styled.form`
     border-radius: var(--border-m);
     transition: var(--transition-short);
     &:focus {
-      outline: var(--color-secondary) 1px solid;
+      outline: var(--color-success) 1px solid;
       color: var(--color-primary);
+    }
+    &:required:invalid {
+      outline: var(--color-danger) 1px solid;
+    }
+    &:required:invalid:not(:focus) {
+      outline: none;
+    }
+    &:required:valid {
+      outline: var(--color-success) 1px solid;
+    }
+    &:required:invalid::-webkit-validation-bubble-message {
+      display: none;
     }
   }
   .submit-btn {
@@ -43,4 +60,29 @@ export const RegisterFormContainer = styled.form`
       transform: scale(1.05);
     }
   }
+`;
+
+export const RegisterFormStatus = styled.div<RegisterStatusProps>`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--margin-s);
+  padding: var(--padding-s);
+  text-align: center;
+  color: var(--color-secondary);
+  font-size: var(---font-size-s);
+  color: ${(props) => {
+    switch (props.registerStatus) {
+      case 'success':
+        return 'var(--color-success)';
+      case 'error':
+        return 'var(--color-danger)';
+      case 'error409':
+        return 'var(--color-danger)';
+      default:
+        return 'var(--color-secondary)';
+    }
+  }};
 `;
