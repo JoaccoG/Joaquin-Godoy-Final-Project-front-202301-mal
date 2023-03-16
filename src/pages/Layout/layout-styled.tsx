6a8748df786b749d801503960ab306c9e15aef0a
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-16 19:46:08 +0100

    Add header component to main element

--- src/pages/Layout/layout-styled.tsx
+++ src/pages/Layout/layout-styled.tsx
@@ -4,4 +4,8 @@ export const Main = styled.main`
   width: 100%;
   min-height: 100vh;
   padding: var(--margin-l);
+
+  @media screen and (min-width: 1024px) {
+    padding-left: calc(var(--margin-l) + 300px);
+  }
 `;
