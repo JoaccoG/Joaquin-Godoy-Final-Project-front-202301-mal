260db7f25424e74cb3f13c3e91efbbf71229327a
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-18 21:13:51 +0100

    Move posts-related types and interfaces

--- src/models/post-model.ts
+++ src/models/post-model.ts
@@ -1,11 +1,20 @@
 import { User } from './user-model';
+import { Game } from './game-model';
 
 export interface Post {
   user: User;
+  game: Game;
   review: string;
   rating: number;
   photo: string;
   likes: number;
   date: Date;
   formattedDate: string;
+}
+
+export type PostForm = Pick<Post, 'game' | 'review' | 'rating' | 'photo'>;
+
+export interface PostResponse {
+  msg: string;
+  post: Post;
 }
