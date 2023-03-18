2098f08d1600011913577c55ada05c100959eee1
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-18 20:10:33 +0100

    Remove home module from router

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../pages/Layout/Layout';
 import Auth from '../pages/Auth/Auth';
-import Home from '../pages/Home/Home';
 import Error from '../pages/Error/Error';
 
 const router = createBrowserRouter([
@@ -17,7 +16,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '',
-        element: <Home />,
+        element: <Auth />,
       },
     ],
   },
