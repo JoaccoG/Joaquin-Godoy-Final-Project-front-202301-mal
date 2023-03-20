24313f806a75f4fbfdcc5b2f75f6f550af3b0b14
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-20 19:05:14 +0100

    Add home page component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,42 @@
-import PostForm from '../../features/posts/PostForm/PostForm';
-import Title from '../../shared/Title/Title';
+import { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { HomeContainer } from './home-styled';
+import Title from '../../shared/Title/Title';
+import PostCard from '../../features/posts/PostCard/PostCard';
+import PostForm from '../../features/posts/PostForm/PostForm';
+import {
+  getAllPosts,
+  selectPostsSlice,
+} from '../../features/posts/posts-slice';
 
 const Home = () => {
+  const dispatch = useAppDispatch();
+  const { posts } = useAppSelector(selectPostsSlice);
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [dispatch]);
+
   return (
     <HomeContainer>
-      <Title
-        text={'Watch other players reviews'}
-        size={'small'}
-        color={'tertiary'}
-      />
-      <article className="home__post-form">
+      <Title text={'Welcome home!'} size={'small'} color={'tertiary'} />
+      <section className="home__post-form">
+        {/*
+        ------------------------------------------------------
+        NOTE: Pasar games acá cuando haga el slice de games...
+        ------------------------------------------------------
+        */}
         <PostForm games={['game1', 'Game 2', 'Game 3']} />
-      </article>
+      </section>
+      <section className="home__posts">
+        <ul className="posts__list">
+          {posts.map((post) => (
+            <li key={`${post.user.username}-${post.game.name}-${post.date}`}>
+              <PostCard post={post} />
+            </li>
+          ))}
+        </ul>
+      </section>
     </HomeContainer>
   );
 };
