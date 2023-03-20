d073865c04fec516b9c9cc21d73c9f7636d8b5dc
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-20 01:03:48 +0100

    Change home page title

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,11 @@ import { HomeContainer } from './home-styled';
 const Home = () => {
   return (
     <HomeContainer>
-      <Title text={'Create a new post'} size={'small'} color={'tertiary'} />
+      <Title
+        text={'Watch other players reviews'}
+        size={'small'}
+        color={'tertiary'}
+      />
       <article className="home__post-form">
         <PostForm games={['game1', 'Game 2', 'Game 3']} />
       </article>
