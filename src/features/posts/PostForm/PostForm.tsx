d7cf5cc1a9a50bc8b1bb809dec9ba622476e76e9
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-20 01:05:25 +0100

    Add post creation feedback component

--- src/features/posts/PostForm/PostForm.tsx
+++ src/features/posts/PostForm/PostForm.tsx
@@ -1,5 +1,5 @@
 import { useAppDispatch, useAppSelector } from '../../../app/hooks';
-import { PostFormContainer } from './post-form-styled';
+import { PostFormContainer, PostFormFeedback } from './post-form-styled';
 import { createNewPost, selectPostsSlice } from '../posts-slice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
@@ -41,11 +41,24 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
   const postFeedback = () => {
     switch (postCreationStatus) {
       case 'idle':
-        return <p>{postCreationMsg}</p>;
+        return (
+          <PostFormFeedback postStatus={postCreationStatus}>
+            Post something new...
+          </PostFormFeedback>
+        );
       case 'success':
-        return <p>Post successfully created!</p>;
+        return (
+          <PostFormFeedback postStatus={postCreationStatus}>
+            Your post has been created!
+          </PostFormFeedback>
+        );
       case 'error':
-        return <p>{postCreationMsg}</p>;
+        return (
+          <PostFormFeedback postStatus={postCreationStatus}>
+            Error during post creation, please try again later. (
+            {postCreationMsg})
+          </PostFormFeedback>
+        );
     }
   };
 
@@ -60,94 +73,96 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
   };
 
   return (
-    <PostFormContainer
-      onSubmit={(e) => {
-        e.preventDefault();
-        dispatch(createNewPost(e.currentTarget));
-        resetFormFields();
-      }}
-      textareaHeight={textareaHeight}
-      data-testid="posts-form"
-      ref={formRef}
-    >
-      {status === 'loading' ? (
-        <Spinner color="tertiary" size={150} />
-      ) : (
-        <>
-          {postFeedback()}
-          <div className="post-form__wrapper-1">
-            <label htmlFor="game">
-              <select
-                id="game"
-                name="game"
-                required
-                defaultValue={''}
-                data-testid="game"
-              >
-                <option value="" disabled>
-                  Choose a game
-                </option>
-                {games.map((game) => (
-                  <option key={game} value={game}>
-                    {game}
+    <>
+      <PostFormContainer
+        ref={formRef}
+        textareaHeight={textareaHeight}
+        data-testid="posts-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch(createNewPost(e.currentTarget));
+          resetFormFields();
+        }}
+      >
+        {status === 'loading' ? (
+          <Spinner color="tertiary" size={150} />
+        ) : (
+          <>
+            {postFeedback()}
+            <div className="post-form__wrapper-1">
+              <label htmlFor="game">
+                <select
+                  id="game"
+                  name="game"
+                  required
+                  defaultValue={''}
+                  data-testid="game"
+                >
+                  <option value="" disabled>
+                    Choose a game
                   </option>
-                ))}
-              </select>
-            </label>
-            <label htmlFor="rating">
-              <select
-                id="rating"
-                name="rating"
-                required
-                defaultValue={''}
-                data-testid="rating"
-              >
-                <option value="" disabled>
-                  Rate your game
-                </option>
-                {Array.from({ length: 5 }, (_asd, i) => (
-                  <option key={i} value={i + 1}>
-                    {i + 1}
+                  {games.map((game) => (
+                    <option key={game} value={game}>
+                      {game}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <label htmlFor="rating">
+                <select
+                  id="rating"
+                  name="rating"
+                  required
+                  defaultValue={''}
+                  data-testid="rating"
+                >
+                  <option value="" disabled>
+                    Rate your game
                   </option>
-                ))}
-              </select>
-            </label>
-          </div>
-          <label htmlFor="review">
-            <textarea
-              required
-              id="review"
-              name="review"
-              placeholder="Tell others your experience..."
-              maxLength={300}
-              value={textareaValue}
-              ref={textareaRef}
-              onChange={handleTextareaHeight}
-              data-testid="review"
-            />
-          </label>
-          <div className="post-form__wrapper-2">
-            <label htmlFor="photo">
-              <FontAwesomeIcon
-                className="wrapper-2__file-upload-icon"
-                icon={solid('image')}
-              />
-              {fileName && <span>{fileName}</span>}
-              <input
-                type="file"
-                name="photo"
-                id="photo"
-                accept="image/jpeg,image/jpg,image/png,image/webp"
-                ref={fileInputRef}
-                onChange={handleFileSelection}
-                data-testid="photo"
+                  {Array.from({ length: 5 }, (_asd, i) => (
+                    <option key={i} value={i + 1}>
+                      {i + 1}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
+            <label htmlFor="review">
+              <textarea
+                required
+                id="review"
+                name="review"
+                placeholder="Tell others your experience..."
+                maxLength={300}
+                value={textareaValue}
+                ref={textareaRef}
+                onChange={handleTextareaHeight}
+                data-testid="review"
               />
             </label>
-            <button type="submit">Post</button>
-          </div>
-        </>
-      )}
-    </PostFormContainer>
+            <div className="post-form__wrapper-2">
+              <label htmlFor="photo">
+                <FontAwesomeIcon
+                  className="wrapper-2__file-upload-icon"
+                  icon={solid('image')}
+                />
+                {fileName && <span>{fileName}</span>}
+                <input
+                  type="file"
+                  name="photo"
+                  id="photo"
+                  accept="image/jpeg,image/jpg,image/png,image/webp"
+                  ref={fileInputRef}
+                  onChange={handleFileSelection}
+                  data-testid="photo"
+                />
+              </label>
+              <button type="submit">Post</button>
+            </div>
+          </>
+        )}
+      </PostFormContainer>
+    </>
   );
 };
 
