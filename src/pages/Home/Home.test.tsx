0f55b2f056c483b4699709d7efb146aed45b9f2c
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-20 19:05:23 +0100

    Add home page component tests

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,16 +1,49 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import { store } from '../../app/store';
+import { errorHandlers } from '../../mocks/handlers';
+import { server } from '../../mocks/server';
 import Home from './Home';
 
-describe('Given an error page', () => {
-  test('When the page renders, then it should render its child elements', () => {
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    );
-    expect(screen.getByRole('heading')).toBeInTheDocument();
-    expect(screen.getByRole('article')).toBeInTheDocument();
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('Given a home page', () => {
+  describe('When the page renders and there are no posts to get', () => {
+    server.use(...errorHandlers);
+    test('When the page renders but there are an error while getting posts, then the response should be an error', async () => {
+      render(
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      );
+
+      try {
+        await screen.findAllByRole('listitem');
+      } catch {}
+
+      await waitFor(() => {
+        expect(screen.queryByRole('listitem')).toBe(null);
+      });
+    });
+  });
+  describe('When the page renders with the right data', () => {
+    test('When the page renders and there are posts to get, then there should be listitems in the document', async () => {
+      render(
+        <Provider store={store}>
+          <MemoryRouter>
+            <Home />
+          </MemoryRouter>
+        </Provider>
+      );
+
+      const listElements = await screen.findAllByRole('listitem');
+
+      await waitFor(() => {
+        expect(listElements[0]).toBeInTheDocument();
+      });
+    });
   });
 });
