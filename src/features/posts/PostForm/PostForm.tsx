5193d1c6706a3458e8c650c29b4ad780ad224761
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-20 19:06:01 +0100

    Refactor posts form creator

--- src/features/posts/PostForm/PostForm.tsx
+++ src/features/posts/PostForm/PostForm.tsx
@@ -3,7 +3,7 @@ import { PostFormContainer, PostFormFeedback } from './post-form-styled';
 import { createNewPost, selectPostsSlice } from '../posts-slice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
-import { FC, useRef, useState } from 'react';
+import { FC, useRef } from 'react';
 import Spinner from '../../../shared/Loading/Loading';
 
 interface PostFormProps {
@@ -14,29 +14,7 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
   const dispatch = useAppDispatch();
   const { status, postCreationStatus, postCreationMsg } =
     useAppSelector(selectPostsSlice);
-  const [textareaValue, setTextareaValue] = useState('');
-  const [textareaHeight, setTextareaHeight] = useState(32);
-  const [fileName, setFileName] = useState('File name');
   const formRef = useRef<HTMLFormElement>(null);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleTextareaHeight = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setTextareaValue(event.target.value);
-    setTextareaHeight(event.target.scrollHeight);
-    if (event.target.value === '') {
-      setTextareaHeight(32);
-    }
-  };
-
-  const handleFileSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = event.target.files?.[0];
-    if (selectedFile) {
-      setFileName(selectedFile.name);
-    }
-  };
 
   const postFeedback = () => {
     switch (postCreationStatus) {
@@ -62,26 +40,14 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
     }
   };
 
-  const resetFormFields = () => {
-    formRef.current?.reset();
-    setTextareaValue('');
-    setTextareaHeight(32);
-    if (fileInputRef.current) {
-      fileInputRef.current.value = '';
-    }
-    setFileName('File name');
-  };
-
   return (
     <>
       <PostFormContainer
         ref={formRef}
-        textareaHeight={textareaHeight}
         data-testid="posts-form"
         onSubmit={(e) => {
           e.preventDefault();
           dispatch(createNewPost(e.currentTarget));
-          resetFormFields();
         }}
       >
         {status === 'loading' ? (
@@ -91,36 +57,32 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
             {postFeedback()}
             <div className="post-form__wrapper-1">
               <label htmlFor="game">
-                <select
-                  id="game"
-                  name="game"
-                  required
-                  defaultValue={''}
-                  data-testid="game"
-                >
+                <select id="game" name="game" required data-testid="game">
                   <option value="" disabled>
                     Choose a game
                   </option>
                   {games.map((game) => (
-                    <option key={game} value={game}>
+                    <option
+                      key={game}
+                      value={game}
+                      data-testid={`game-${game}`}
+                    >
                       {game}
                     </option>
                   ))}
                 </select>
               </label>
               <label htmlFor="rating">
-                <select
-                  id="rating"
-                  name="rating"
-                  required
-                  defaultValue={''}
-                  data-testid="rating"
-                >
+                <select id="rating" name="rating" required data-testid="rating">
                   <option value="" disabled>
                     Rate your game
                   </option>
                   {Array.from({ length: 5 }, (_asd, i) => (
-                    <option key={i} value={i + 1}>
+                    <option
+                      key={i + 1}
+                      value={i + 1}
+                      data-testid={`rating-${i + 1}`}
+                    >
                       {i + 1}
                     </option>
                   ))}
@@ -134,9 +96,6 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
                 name="review"
                 placeholder="Tell others your experience..."
                 maxLength={300}
-                value={textareaValue}
-                ref={textareaRef}
-                onChange={handleTextareaHeight}
                 data-testid="review"
               />
             </label>
@@ -146,14 +105,11 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
                   className="wrapper-2__file-upload-icon"
                   icon={solid('image')}
                 />
-                {fileName && <span>{fileName}</span>}
                 <input
                   type="file"
                   name="photo"
                   id="photo"
                   accept="image/jpeg,image/jpg,image/png,image/webp"
-                  ref={fileInputRef}
-                  onChange={handleFileSelection}
                   data-testid="photo"
                 />
               </label>
