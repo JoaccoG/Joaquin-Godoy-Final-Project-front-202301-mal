bf1d070615e5369facbe782022af022f0e44177f
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-22 20:43:43 +0100

    Remvoe loading from posts form due to repeated loading on posts list

--- src/features/posts/PostForm/PostForm.tsx
+++ src/features/posts/PostForm/PostForm.tsx
@@ -4,7 +4,6 @@ import { createNewPost, selectPostsSlice } from '../posts-slice';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { FC } from 'react';
-import Spinner from '../../../shared/Loading/Loading';
 
 interface PostFormProps {
   games: string[];
@@ -12,7 +11,7 @@ interface PostFormProps {
 
 const PostForm: FC<PostFormProps> = ({ games }) => {
   const dispatch = useAppDispatch();
-  const { status, postCreationStatus, postCreationMsg } =
+  const { postCreationStatus, postCreationMsg } =
     useAppSelector(selectPostsSlice);
 
   const postFeedback = () => {
@@ -48,85 +47,77 @@ const PostForm: FC<PostFormProps> = ({ games }) => {
           dispatch(createNewPost(e.currentTarget));
         }}
       >
-        {status === 'loading' ? (
-          <Spinner color="tertiary" size={150} />
-        ) : (
-          <>
-            {postFeedback()}
-            <div className="post-form__wrapper-1">
-              <label htmlFor="game">
-                <select
-                  id="game"
-                  name="game"
-                  required
-                  defaultValue=""
-                  data-testid="game"
-                >
-                  <option value="" disabled>
-                    Choose a game
-                  </option>
-                  {games.map((game) => (
-                    <option
-                      key={game}
-                      value={game}
-                      data-testid={`game-${game}`}
-                    >
-                      {game}
-                    </option>
-                  ))}
-                </select>
-              </label>
-              <label htmlFor="rating">
-                <select
-                  id="rating"
-                  name="rating"
-                  required
-                  defaultValue=""
-                  data-testid="rating"
-                >
-                  <option value="" disabled>
-                    Rate your game
-                  </option>
-                  {Array.from({ length: 5 }, (_asd, i) => (
-                    <option
-                      key={i + 1}
-                      value={i + 1}
-                      data-testid={`rating-${i + 1}`}
-                    >
-                      {i + 1}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-            <label htmlFor="review">
-              <textarea
+        <>
+          {postFeedback()}
+          <div className="post-form__wrapper-1">
+            <label htmlFor="game">
+              <select
+                id="game"
+                name="game"
                 required
-                id="review"
-                name="review"
-                placeholder="Tell others your experience..."
-                maxLength={300}
-                data-testid="review"
+                defaultValue=""
+                data-testid="game"
+              >
+                <option value="" disabled>
+                  Choose a game
+                </option>
+                {games.map((game) => (
+                  <option key={game} value={game} data-testid={`game-${game}`}>
+                    {game}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <label htmlFor="rating">
+              <select
+                id="rating"
+                name="rating"
+                required
+                defaultValue=""
+                data-testid="rating"
+              >
+                <option value="" disabled>
+                  Rate your game
+                </option>
+                {Array.from({ length: 5 }, (_asd, i) => (
+                  <option
+                    key={i + 1}
+                    value={i + 1}
+                    data-testid={`rating-${i + 1}`}
+                  >
+                    {i + 1}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <label htmlFor="review">
+            <textarea
+              required
+              id="review"
+              name="review"
+              placeholder="Tell others your experience..."
+              maxLength={300}
+              data-testid="review"
+            />
+          </label>
+          <div className="post-form__wrapper-2">
+            <label htmlFor="photo">
+              <FontAwesomeIcon
+                className="wrapper-2__file-upload-icon"
+                icon={solid('image')}
+              />
+              <input
+                type="file"
+                name="photo"
+                id="photo"
+                accept="image/jpeg,image/jpg,image/png,image/webp"
+                data-testid="photo"
               />
             </label>
-            <div className="post-form__wrapper-2">
-              <label htmlFor="photo">
-                <FontAwesomeIcon
-                  className="wrapper-2__file-upload-icon"
-                  icon={solid('image')}
-                />
-                <input
-                  type="file"
-                  name="photo"
-                  id="photo"
-                  accept="image/jpeg,image/jpg,image/png,image/webp"
-                  data-testid="photo"
-                />
-              </label>
-              <button type="submit">Post</button>
-            </div>
-          </>
-        )}
+            <button type="submit">Post</button>
+          </div>
+        </>
       </PostFormContainer>
     </>
   );
