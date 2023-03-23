f7c3ec6fb7250ca851d178b73d11c8eac885aafb
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-23 01:48:08 +0100

    Add infinite scroll tests

--- src/features/posts/PostCardList/PostCardList.test.tsx
+++ src/features/posts/PostCardList/PostCardList.test.tsx
@@ -1,9 +1,8 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { store } from '../../../app/store';
 import { errorHandlers } from '../../../mocks/handlers';
 import { server } from '../../../mocks/server';
+import { renderWithProviders } from '../../../mocks/utils';
 import PostCardList from './PostCardList';
 
 beforeAll(() => server.listen());
@@ -14,10 +13,10 @@ describe('Given a posts card list component', () => {
   describe('When the page renders and there are no posts to get', () => {
     server.use(...errorHandlers);
     test('Then the response should be an error', async () => {
-      render(
-        <Provider store={store}>
+      renderWithProviders(
+        <MemoryRouter>
           <PostCardList />
-        </Provider>
+        </MemoryRouter>
       );
 
       try {
@@ -29,20 +28,37 @@ describe('Given a posts card list component', () => {
       });
     });
   });
+
   describe('When the page renders with the right data', () => {
     test('Then there should be listitems in the document', async () => {
-      render(
-        <Provider store={store}>
-          <MemoryRouter>
-            <PostCardList />
-          </MemoryRouter>
-        </Provider>
+      renderWithProviders(
+        <MemoryRouter>
+          <PostCardList />
+        </MemoryRouter>
       );
 
       const cardsList = await screen.findAllByRole('listitem');
 
       await waitFor(() => {
         expect(cardsList[0]).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('When the user scrolls to see more posts', () => {
+    test('Then more posts should load', async () => {
+      renderWithProviders(
+        <MemoryRouter>
+          <PostCardList />
+        </MemoryRouter>
+      );
+
+      expect(screen.getByTestId('spinner')).toBeInTheDocument();
+      // expect(await screen.findAllByRole('listitem')).toHaveLength(4);
+
+      await fireEvent.scroll(window, { EventTarget: { scrollY: 1000 } });
+      await waitFor(() => {
+        expect(screen.getAllByRole('listitem')).toHaveLength(12);
       });
     });
   });
