ecb96211eebed82400a4f52bf4eb44423d38ab8d
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-23 16:12:09 +0100

    Add file preview test

--- src/features/posts/components/PostForm/PostForm.test.tsx
+++ src/features/posts/components/PostForm/PostForm.test.tsx
@@ -1,9 +1,8 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 import { server } from '../../../../mocks/server';
-import { Provider } from 'react-redux';
-import { store } from '../../../../app/store';
 import PostForm from './PostForm';
 import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from '../../../../mocks/utils';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -12,12 +11,24 @@ afterAll(() => server.close());
 describe('Given a posts form component', () => {
   describe('When the component is render', () => {
     test('Then there should be a form in the document', () => {
-      render(
-        <Provider store={store}>
-          <PostForm games={['game-1', 'game-2']} />
-        </Provider>
-      );
+      renderWithProviders(<PostForm games={['game-1', 'game-2']} />);
       expect(screen.getByTestId('posts-form')).toBeInTheDocument();
+    });
+
+    test('And the user uploads a file, he should see the name of the file he is trying to upload', async () => {
+      renderWithProviders(<PostForm games={['game-1', 'game-2']} />);
+      expect(screen.queryByText(/test/i)).not.toBeInTheDocument();
+
+      const blob = new Blob([''], { type: 'image/png' });
+      const file = new File([blob], 'test.png', { type: 'image/png' });
+      await fireEvent.change(screen.getByTestId('photo'), {
+        target: {
+          files: [file, 'test.png'],
+        },
+      });
+      await waitFor(() => {
+        expect(screen.getByText(/test/i)).toBeInTheDocument();
+      });
     });
   });
 
@@ -28,18 +39,14 @@ describe('Given a posts form component', () => {
     });
 
     test('With valid data, then it should show a success feedback message', async () => {
-      /* FIX: Not being able to mock multipart/form-data api requests with MSW. Mocked the fetch return value instead */
+      /* FIXME: Not being able to mock multipart/form-data api requests with MSW. Mocked the fetch return value instead */
       globalThis.fetch = jest.fn().mockResolvedValue({
         ok: true,
         json: jest.fn().mockResolvedValue({
           msg: 'Your post has been created!',
         }),
       });
-      render(
-        <Provider store={store}>
-          <PostForm games={['game-1', 'game-2']} />
-        </Provider>
-      );
+      renderWithProviders(<PostForm games={['game-1', 'game-2']} />);
 
       userEvent.selectOptions(screen.getByTestId('game'), 'game-1');
       userEvent.selectOptions(screen.getByTestId('rating'), '3');
@@ -48,35 +55,25 @@ describe('Given a posts form component', () => {
       userEvent.click(screen.getByRole('button'));
 
       await waitFor(() => {
-        expect(
-          screen.getByText('Your post has been created!')
-        ).toBeInTheDocument();
+        expect(screen.getByText(/created/i)).toBeInTheDocument();
       });
     });
 
     test('With wrong data, then it should be an error feedback message', async () => {
-      /* FIX: Not being able to mock multipart/form-data api requests with MSW. Mocked the fetch return value instead */
+      /* FIXME: Not being able to mock multipart/form-data api requests with MSW. Mocked the fetch return value instead */
       globalThis.fetch = jest.fn().mockResolvedValue({
         ok: false,
         json: jest.fn().mockResolvedValue({
           msg: 'There was an error during your request',
         }),
       });
-      render(
-        <Provider store={store}>
-          <PostForm games={['game-1', 'game-2']} />
-        </Provider>
-      );
+      renderWithProviders(<PostForm games={['game-1', 'game-2']} />);
 
       userEvent.selectOptions(screen.getByTestId('game'), 'game-1');
       userEvent.click(screen.getByRole('button'));
 
       await waitFor(() => {
-        expect(
-          screen.getByText(
-            'Error during post creation, please try again later. (There was an error during your request)'
-          )
-        ).toBeInTheDocument();
+        expect(screen.getByText(/error/i)).toBeInTheDocument();
       });
     });
   });
