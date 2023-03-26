c286f5d60bbef279402dda136591c312b5b245d4
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-26 18:29:39 +0200

    Add api call to get one user posts

--- src/features/user/users/users-api.ts
+++ src/features/user/users/users-api.ts
@@ -11,3 +11,21 @@ export const getUserById = async (userId: string) => {
 
   return apiRes;
 };
+
+export const getPostsByUserId = async (
+  userId: string,
+  offset: number,
+  limit: number
+) => {
+  const response = await fetch(
+    `${process.env.REACT_APP_API_URL}/api/v1/users/${userId}/posts?offset=${offset}&limit=${limit}`,
+    {
+      headers: {
+        Authorization: `Bearer ${sessionStorage.getItem('accessToken')}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  return response;
+};
