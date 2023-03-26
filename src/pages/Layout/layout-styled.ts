e0d9accdfc9d2a9b8227f2be4e8196f91674e552
JoaccoG <joaquingodoy2407@gmail.com>
2023-03-26 20:12:12 +0200

    Fix variables of layout padding

--- src/pages/Layout/layout-styled.ts
+++ src/pages/Layout/layout-styled.ts
@@ -3,10 +3,10 @@ import styled from 'styled-components';
 export const Main = styled.main`
   width: 100%;
   min-height: 100vh;
-  padding: var(--margin-xxxl) var(--margin-m) var(--margin-l);
+  padding: var(--padding-xxxl) var(--padding-m) var(--padding-l);
 
   @media screen and (min-width: 1024px) {
-    padding: var(--margin-l) var(--margin-l) var(--margin-l)
-      calc(var(--margin-l) + 300px);
+    padding: var(--padding-l) var(--padding-l) var(--padding-l)
+      calc(var(--padding-l) + 300px);
   }
 `;
